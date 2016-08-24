e8edcf350f0528840059c2ec0b94070536f00053
sclrl220 <sclrl220@gmail.com>
2016-08-24 14:39:24 +0800

    12

--- src/main/java/com/sobey/po/Employee.java
+++ src/main/java/com/sobey/po/Employee.java
@@ -18,42 +18,48 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * 〈一句话功能简述〉
- * 〈功能详细描述〉
+ * 用户实体
+ *
  * @author [liruilang]
  * @version 2015年12月22日-下午5:58:18
  * @see Employee
  */
 @Entity
-@Table(name="lrl_employee")
-public class Employee implements Serializable{
+@Table(name = "lrl_employee")
+public class Employee implements Serializable {
     private Integer empId;
     private String name;
     private String password;
-    
+
     @Id
-    @Column(name="id")
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer getEmpId() {
         return empId;
     }
+
     public void setEmpId(Integer empId) {
         this.empId = empId;
     }
-    @Column(name="emp_name")
+
+    @Column(name = "emp_name")
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    @Column(name="emp_password")
+
+    @Column(name = "emp_password")
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     @Override
     public String toString() {
         return "Employee [empId=" + empId + ", name=" + name + ", password=" + password + "]";
